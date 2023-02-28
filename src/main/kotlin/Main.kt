6b847934bd1482efc7a6cb2a81ce1b00d4cf38d7
ahmetonay <ahmet68onay@gmail.com>
2023-02-28 23:00:58 +0100

    programmed number guessing game

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,27 @@
+import kotlin.random.Random
+
+fun main() {
+    var random: Int
+    do {
+        random = Random.nextInt(1000,10000)
+    } while (!random.isEachDigitUnique())
+
+
+    println(random)
+    do{
+        print("Guess the 4 Digits: ")
+        val rand = random.toString().toCharArray().toList()
+        val res = readLine().toString().toCharArray().toList()
+        val resTemp = res.toMutableList()
+
+        val countRightByPosition = rand.zip(res) .count{ (a,b) -> a == b }
+        val countRight = rand.filter { it in resTemp }.also { i -> resTemp.removeAll(i) }.count()
+        println("User Input: ${res.joinToString("")}, Output: $countRight:$countRightByPosition")
+    }while(res !=rand)
+
+}
+
+fun Int.isEachDigitUnique(): Boolean {
+    val digits = this.toString().toCharArray()
+    return digits.toSet().size == digits.size
+}
